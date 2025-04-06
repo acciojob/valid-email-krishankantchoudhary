c8ff38e2fd0f810d4480bdaee620a0c099109aad
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-06 12:57:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validEmail(str) {
   //your JS code here.
+	if(!str){
+		return false;
+	}
+	const regex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+	return regex.test(str);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
